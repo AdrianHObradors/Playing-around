ee4dfd132b86809aba77bed8e030145aa46c0035
Adrián Hernández Obradors <33419732+AdrianHObradors@users.noreply.github.com>
2017-11-06 21:57:08 +0100

    About things I find about on python.

---
+++ thingsIfindof.py
@@ -0,0 +1,3 @@
+string.index("stringhere") #gives position of "stringhere" in string. Works also with arrays. 
+
+string.count("stringhere") #gives amount   of "stringhere" in string. Works also with arrays
